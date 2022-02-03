308f13bacafec9f51961df9c670bece9a13302cb
LAPTOP-RJHC46RB\seyeo <seyeon3178@duksung.ac.kr>
2022-02-03 14:07:36 +0900

    ..

---
+++ CalculatorStudy/src/Pnum.java
@@ -0,0 +1,14 @@
+
+public class Pnum {
+	
+	String hec,oct,bin;
+	int dex;
+	
+	Pnum(String hec,int dex,String oct,String bin){
+		this.hec=hec;
+		this.dex=dex;
+		this.oct=oct;
+		this.bin=bin;
+	}
+	
+}

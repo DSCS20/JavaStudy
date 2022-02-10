70aeb70db3a4f1b9aa3158f15c2e8dfdd1ea6794
Ohjunghh <20200685@duksung.ac.kr>
2022-02-10 14:10:56 +0900

    GUI

--- CalculatorStudy/src/DateCalc.java
+++ CalculatorStudy/src/DateCalc.java
@@ -7,7 +7,6 @@ import java.util.Locale;
 import java.time.temporal.ChronoUnit;
 
 
-
 //java.util.Date > java.util.Calendar > java.time
 
 
@@ -18,22 +17,22 @@ public class DateCalc extends Calculator implements Dnum{
 	//날짜 더하거나 뺄 때
 	
 		//더하거나 뺄 연 월 일
-	static int ay=1; //addyear
-	static int am=1;
-	static int ad=1;
+	static int ay=0; //addyear
+	static int am=0;
+	static int ad=0;
 	
 	static int getyear=0;//추가or빼서 얻어올 연도
 	static int getmonth=0;//추가or빼서 얻어올 월			
 	static int getdays=0;//추가or빼서 얻어올 일
 			
 	static int week=0;//getdays가 7이상이면 1주로 바꾸기
-	static long getalldays=0;//총 며칠인지 계산하기			
+	static int getalldays=0;//총 며칠인지 계산하기			
 	static String getd=" " ; //얻어올 요일s
 			
 		
 		//날짜 차이 구할 때
 			
-			//날짜 차이 구할때 시작과 끝의 연 월 일 알아와야 계산 가능
+	//날짜 차이 구할때 시작과 끝의 연 월 일 알아와야 계산 가능
 	static int syear=1;
 	static int smonth=1;	
 	static int sday=1;
@@ -46,9 +45,10 @@ public class DateCalc extends Calculator implements Dnum{
 	static int month=0;
 	static int day=0;
 	
-	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년MM월dd일");
+	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" yyyy년 MM월 dd일 ");
 	static LocalDate toDateTime=LocalDate.now();
 	static LocalDate fromDateTime=LocalDate.now();
+	static LocalDate startDate=LocalDate.now();
 
 	public static void FromToDate(LocalDate toDateTime, LocalDate fromDateTime) {
 		
@@ -58,40 +58,36 @@ public class DateCalc extends Calculator implements Dnum{
 			getyear=period.getYears();	getyear=getyear<0?-getyear:getyear;
 			getmonth=period.getMonths();	getmonth=getmonth<0?-getmonth:getmonth;
 			getdays=period.getDays();	getdays=getdays<0?-getdays:getdays;
-			getalldays=ChronoUnit.DAYS.between(toDateTime, fromDateTime);
-		
-			 //실제 계산기에서 0이면 안나와서 //gui 이벤트쪽으로 옮기기해야할듯..?
-			System.out.print(toDateTime+"와 "+fromDateTime+"의 차이 : ");
-			if(getyear!=0)
-				System.out.print(getyear+"년, "); //print가 아니라 라벨로 바꿔서 해야함 나중에 이벤트 처리..?
-			if(getmonth!=0)
-				System.out.print(getmonth+"월, ");
-			if(getdays!=0) {
-				if(getdays>=7) {
-					week=getdays/7;
-					getdays-=week*7;
-					System.out.print(week+"주, ");
-				}
-				System.out.print(getdays+"일 ");
+			getalldays=(int)ChronoUnit.DAYS.between(toDateTime, fromDateTime); getalldays=getalldays<0?-getalldays:getalldays;
+			
+			if(getdays>=7) {
+				week=getdays/7;
+				getdays-=week*7;
 			}
-			 System.out.println("(총 "+ getalldays+"일)");
 	}
 
 	
 	
-//public static void AddDate(String date,long year,long month,long day)
-	public static void AddDate(LocalDate date,int year,int month,int day) {
-		
+	//날짜에 더하기 연산
+	public static LocalDate AddDate(LocalDate date,int year,int month,int day) {
+			
 			date=date.plusYears(year); 
 			date=date.plusMonths(month);
 			date=date.plusDays(day); 
+			GetDayOfWeek(date);
+			
+			return date;
 	}
-	
-	public static void MinusDate(LocalDate date,int year,int month,int day) {
+	//날짜에 빼기 연산
+	public static  LocalDate MinusDate(LocalDate date,int year,int month,int day) {
 		  date=date.minusYears(year);
 		  date=date.minusMonths(month);
 		  date=date.minusDays(day);
+		  GetDayOfWeek(date);
+		  
+		  return date;
 	}
+
 	
 	//요일 구하기
 	public static void GetDayOfWeek(LocalDate date) {
--- CalculatorStudy/src/GUI_date.java
+++ CalculatorStudy/src/GUI_date.java
@@ -3,15 +3,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
+
+import java.time.LocalDate;
+
 
 public class GUI_date extends DateCalc {
 			
@@ -45,6 +44,7 @@ public class GUI_date extends DateCalc {
 		JComboBox<Integer> yearCombo = new JComboBox<Integer>();
 		DefaultComboBoxModel<Integer> yearModel = new DefaultComboBoxModel<Integer>(); 
 		JLabel yLbl = new JLabel("년 "); 
+		
 		
 		JComboBox<Integer> monthCombo = new JComboBox<Integer>();
 		DefaultComboBoxModel<Integer> monthModel = new DefaultComboBoxModel<Integer>();
@@ -97,31 +97,7 @@ public class GUI_date extends DateCalc {
 		p.add(dayCombo).setBounds(244,78,60,30); dayCombo.setModel(dayModel); 
 		dayCombo.setSelectedItem(sday); p.add(dLbl).setBounds(306,78,60,30); 
 		
-		yearCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				int y= (int) e.getItem();
-				toDateTime=toDateTime.withYear(y);
-			}
-		});
-		
-		monthCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				int m= (int) e.getItem();
-				toDateTime=toDateTime.withMonth(m);
-			}
-		});
 
-		
-		dayCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				int d= (int) e.getItem();
-				toDateTime=toDateTime.withDayOfMonth(d);
-			}
-		});
-
-
-		
-		System.out.println(toDateTime);
 		JLabel l2=new JLabel("종료일");
 		p.add(l2).setBounds(20,124,40,20);
 		
@@ -135,64 +111,397 @@ public class GUI_date extends DateCalc {
 		p.add(EdayCombo).setBounds(244,120,60,30); EdayCombo.setModel(EdayModel); 
 		EdayCombo.setSelectedItem(eday); p.add(EdLbl).setBounds(306,120,60,30); 
 		
-	
-		EyearCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				int y= (int) e.getItem();
-				fromDateTime=fromDateTime.withYear(y);
-			}
-		});
-		
-		EmonthCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				int m= (int) e.getItem();
-				fromDateTime=fromDateTime.withMonth(m);
-			}
-		});
 
 		
-		EdayCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				int d= (int) e.getItem();
-				fromDateTime=fromDateTime.withDayOfMonth(d);
-			}
-		});
-		
 		JLabel get1=new JLabel("동일한 날짜");
-		p.add(get1).setBounds(80,173,100,20);
+		p.add(get1).setBounds(80,173,190,20);
 		
+		JLabel dif=new JLabel("차이");
+		p.add(dif).setBounds(20,166,60,30);	
 		
-		//JLabel l3=new JLabel("차이");
-		JButton dif=new JButton("차이");
-		p.add(dif).setBounds(10,166,60,30);	
+		JLabel gylb=new JLabel();
+		p.add(gylb);
+		
+		JLabel gmlb=new JLabel();
+		 p.add(gmlb);
 	
-		dif.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		JLabel gwlb=new JLabel();
+		 p.add(gwlb);
+		 
+		JLabel gdlb=new JLabel();
+		 p.add(gdlb);
+		 
+		JLabel gadlb=new JLabel();
+		 p.add(gadlb);
+		
+		 yearCombo.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					int y= (int) e.getItem();
+					toDateTime=toDateTime.withYear(y);
+					
+					int xdistance=100;
+					DateCalc.FromToDate(toDateTime,fromDateTime);
+					
+						
+					if(toDateTime.isEqual(fromDateTime)==true)
+						get1.setVisible(true);
 				
-				DateCalc.FromToDate(toDateTime,fromDateTime);
-				get1.setText(getyear+"년 "+getmonth+"월 "+week+"주 "+getdays+"일");
-				
-				/*
-				 if(getyear!=0)
-				System.out.print(getyear+"년, "); //print가 아니라 라벨로 바꿔서 해야함 나중에 이벤트 처리..?
-			if(getmonth!=0)
-				System.out.print(getmonth+"월, ");
-			if(getdays!=0) {
-				if(getdays>=7) {
-					week=getdays/7;
-					getdays-=week*7;
-					System.out.print(week+"주, ");
+						if(getyear==0) 
+							gylb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gylb.setText(getyear+"년,");
+							 gylb.setBounds(xdistance-10,173,40,20);
+							 gylb.setVisible(true);
+							 xdistance+=40;
+						}
+						if(getmonth==0)
+							gmlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gmlb.setText(getmonth+"월,");
+							gmlb.setBounds(xdistance,173,40,20);
+							gmlb.setVisible(true);
+							xdistance+=40;
+						}
+						
+						if(week==0) 
+							gwlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gwlb.setText(week+"주,");
+							 gwlb.setBounds(xdistance,173,40,20);
+							 gwlb.setVisible(true);
+							 xdistance+=40;
+						}
+						
+						if(getdays==0) 
+							gdlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gdlb.setText(getdays+"일,");
+							gdlb.setBounds(xdistance,173,40,20);
+							gdlb.setVisible(true);
+							xdistance+=40;
+						}
+						if(getalldays==0)
+							gadlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gadlb.setText("총 "+getalldays+"일");
+							gadlb.setBounds(xdistance,173,150,20);
+							gadlb.setVisible(true);
+						}
 				}
-				System.out.print(getdays+"일 ");
-			}
-			 System.out.println("(총 "+ getalldays+"일)");
-				 */
+			});
+			
+			monthCombo.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					int m= (int) e.getItem();
+					toDateTime=toDateTime.withMonth(m);
+					int xdistance=100;
+					DateCalc.FromToDate(toDateTime,fromDateTime);
+					
+						
+					if(toDateTime.isEqual(fromDateTime)==true)
+						get1.setVisible(true);
 				
-			}
-		});
-		
-		
-		
+						if(getyear==0) 
+							gylb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gylb.setText(getyear+"년,");
+							 gylb.setBounds(xdistance-10,173,40,20);
+							 gylb.setVisible(true);
+							 xdistance+=40;
+						}
+						if(getmonth==0)
+							gmlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gmlb.setText(getmonth+"월,");
+							gmlb.setBounds(xdistance,173,40,20);
+							gmlb.setVisible(true);
+							xdistance+=40;
+						}
+						
+						if(week==0) 
+							gwlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gwlb.setText(week+"주,");
+							 gwlb.setBounds(xdistance,173,40,20);
+							 gwlb.setVisible(true);
+							 xdistance+=40;
+						}
+						
+						if(getdays==0) 
+							gdlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gdlb.setText(getdays+"일,");
+							gdlb.setBounds(xdistance,173,40,20);
+							gdlb.setVisible(true);
+							xdistance+=40;
+						}
+						if(getalldays==0)
+							gadlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gadlb.setText("총 "+getalldays+"일");
+							gadlb.setBounds(xdistance,173,150,20);
+							gadlb.setVisible(true);
+						}
+				}
+			});
+
+			
+			dayCombo.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					int d= (int) e.getItem();
+					toDateTime=toDateTime.withDayOfMonth(d);
+					int xdistance=100;
+					DateCalc.FromToDate(toDateTime,fromDateTime);
+					
+						
+					if(toDateTime.isEqual(fromDateTime)==true)
+						get1.setVisible(true);
+				
+						if(getyear==0) 
+							gylb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gylb.setText(getyear+"년,");
+							 gylb.setBounds(xdistance-10,173,40,20);
+							 gylb.setVisible(true);
+							 xdistance+=40;
+						}
+						if(getmonth==0)
+							gmlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gmlb.setText(getmonth+"월,");
+							gmlb.setBounds(xdistance,173,40,20);
+							gmlb.setVisible(true);
+							xdistance+=40;
+						}
+						
+						if(week==0) 
+							gwlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gwlb.setText(week+"주,");
+							 gwlb.setBounds(xdistance,173,40,20);
+							 gwlb.setVisible(true);
+							 xdistance+=40;
+						}
+						
+						if(getdays==0) 
+							gdlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gdlb.setText(getdays+"일,");
+							gdlb.setBounds(xdistance,173,40,20);
+							gdlb.setVisible(true);
+							xdistance+=40;
+						}
+						if(getalldays==0)
+							gadlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gadlb.setText("총 "+getalldays+"일");
+							gadlb.setBounds(xdistance,173,150,20);
+							gadlb.setVisible(true);
+						}
+				}
+			});
+
+		 
+		 EyearCombo.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					int y= (int) e.getItem();
+					fromDateTime=fromDateTime.withYear(y);
+					int xdistance=100;
+					DateCalc.FromToDate(toDateTime,fromDateTime);
+					
+						
+					if(toDateTime.isEqual(fromDateTime)==true)
+						get1.setVisible(true);
+				
+						if(getyear==0) 
+							gylb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gylb.setText(getyear+"년,");
+							 gylb.setBounds(xdistance-10,173,40,20);
+							 gylb.setVisible(true);
+							 xdistance+=40;
+						}
+						if(getmonth==0)
+							gmlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gmlb.setText(getmonth+"월,");
+							gmlb.setBounds(xdistance,173,40,20);
+							gmlb.setVisible(true);
+							xdistance+=40;
+						}
+						
+						if(week==0) 
+							gwlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gwlb.setText(week+"주,");
+							 gwlb.setBounds(xdistance,173,40,20);
+							 gwlb.setVisible(true);
+							 xdistance+=40;
+						}
+						
+						if(getdays==0) 
+							gdlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gdlb.setText(getdays+"일,");
+							gdlb.setBounds(xdistance,173,40,20);
+							gdlb.setVisible(true);
+							xdistance+=40;
+						}
+						if(getalldays==0)
+							gadlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gadlb.setText("총 "+getalldays+"일");
+							gadlb.setBounds(xdistance,173,150,20);
+							gadlb.setVisible(true);
+						}
+				}
+			});
+			
+			EmonthCombo.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					int m= (int) e.getItem();
+					fromDateTime=fromDateTime.withMonth(m);
+					int xdistance=100;
+					DateCalc.FromToDate(toDateTime,fromDateTime);
+					
+						
+					if(toDateTime.isEqual(fromDateTime)==true)
+						get1.setVisible(true);
+				
+						if(getyear==0) 
+							gylb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gylb.setText(getyear+"년,");
+							 gylb.setBounds(xdistance-10,173,40,20);
+							 gylb.setVisible(true);
+							 xdistance+=40;
+						}
+						if(getmonth==0)
+							gmlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gmlb.setText(getmonth+"월,");
+							gmlb.setBounds(xdistance,173,40,20);
+							gmlb.setVisible(true);
+							xdistance+=40;
+						}
+						
+						if(week==0) 
+							gwlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gwlb.setText(week+"주,");
+							 gwlb.setBounds(xdistance,173,40,20);
+							 gwlb.setVisible(true);
+							 xdistance+=40;
+						}
+						
+						if(getdays==0) 
+							gdlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gdlb.setText(getdays+"일,");
+							gdlb.setBounds(xdistance,173,40,20);
+							gdlb.setVisible(true);
+							xdistance+=40;
+						}
+						if(getalldays==0)
+							gadlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gadlb.setText("총 "+getalldays+"일");
+							gadlb.setBounds(xdistance,173,150,20);
+							gadlb.setVisible(true);
+						}
+				}
+			});
+
+			
+			EdayCombo.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					int d= (int) e.getItem();
+					fromDateTime=fromDateTime.withDayOfMonth(d);
+					int xdistance=100;
+					DateCalc.FromToDate(toDateTime,fromDateTime);
+					
+						
+					if(toDateTime.isEqual(fromDateTime)==true)
+						get1.setVisible(true);
+				
+						if(getyear==0) 
+							gylb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gylb.setText(getyear+"년,");
+							 gylb.setBounds(xdistance-10,173,40,20);
+							 gylb.setVisible(true);
+							 xdistance+=40;
+						}
+						if(getmonth==0)
+							gmlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gmlb.setText(getmonth+"월,");
+							gmlb.setBounds(xdistance,173,40,20);
+							gmlb.setVisible(true);
+							xdistance+=40;
+						}
+						
+						if(week==0) 
+							gwlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							 gwlb.setText(week+"주,");
+							 gwlb.setBounds(xdistance,173,40,20);
+							 gwlb.setVisible(true);
+							 xdistance+=40;
+						}
+						
+						if(getdays==0) 
+							gdlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gdlb.setText(getdays+"일,");
+							gdlb.setBounds(xdistance,173,40,20);
+							gdlb.setVisible(true);
+							xdistance+=40;
+						}
+						if(getalldays==0)
+							gadlb.setVisible(false);
+						else {
+							get1.setVisible(false);
+							gadlb.setText("총 "+getalldays+"일");
+							gadlb.setBounds(xdistance,173,150,20);
+							gadlb.setVisible(true);
+						}
+				}
+			});
+		 
+		//해야 할 것!!!
+		//이벤트처리에서  겹치는 거 줄이기 - 그룹화?
+		//오류 뜨는거 4월이 30일까진데 31클릭하면 오류뜸 그래서 오류처리하삼
+		//다 하고나면 폰트랑 글씨 크기랑 글씨 위치 바꾸기 + 배경색
 		
 		
 		
@@ -230,6 +539,7 @@ public class GUI_date extends DateCalc {
 	
 		
 		
+		
 		p2.add(yearCombo2).setBounds(80,78,60,30); yearCombo2.setModel(yearModel2); 
 		yearCombo2.setSelectedItem(year); p2.add(yLbl2).setBounds(142,78,60,30); 
 		
@@ -257,49 +567,158 @@ public class GUI_date extends DateCalc {
 		
 
 		p2.add(yearCombo3).setBounds(20,182,45,20); yearCombo3.setModel(yearModel3); 
-		yearCombo3.setSelectedItem(ay);
-		ay=(int) yearCombo3.getSelectedItem(); //	addyear 
 		
-	
 		p2.add(monthCombo3).setBounds(80,182,45,20); monthCombo3.setModel(monthModel3); 
-		monthCombo3.setSelectedItem(am); 
-		am=(int) monthCombo3.getSelectedItem(); 
-		
 		
 		p2.add(dayCombo3).setBounds(140,182,45,20); dayCombo3.setModel(dayModel3); 
-		dayCombo3.setSelectedItem(ad);
-		ad=(int) dayCombo3.getSelectedItem(); 
+		
+	
+		LocalDate nw=LocalDate.now();
+		GetDayOfWeek(nw);
+		JLabel get=new JLabel(nw.format(formatter)+getd);
+		p2.add(get).setBounds(20,240,160,30);
+	
+		
+		
 		
 		JRadioButton radio[]=new JRadioButton[2];
 		
-		//이벤트처리 나중에
-		class MyItemListener implements ItemListener{
-	        @Override
-	        public void itemStateChanged(ItemEvent e) {
-	            if(e.getStateChange()==ItemEvent.DESELECTED){
-	                return;
-	            }
-	            if(radio[0].isSelected()&&!radio[1].isSelected()){
-	                
-	            }
-	            else if(radio[1].isSelected()&&!radio[0].isSelected()){
-	                
-	            }
-	          
-	        }
-		}
 		
 		radio[0]=new JRadioButton("추가");
 		p2.add(radio[0]).setBounds(20,122,50,20);
-		radio[0].addItemListener(new MyItemListener());
 
 		
 		radio[1]=new JRadioButton("빼기");
 		p2.add(radio[1]).setBounds(80,122,50,20);
-		radio[1].addItemListener(new MyItemListener());
+	
 		
-
 		radio[0].setSelected(true);
+		
+		
+		//시작일 선택하는 거 이벤트
+				yearCombo2.addItemListener(new ItemListener() {
+					public void itemStateChanged(ItemEvent e) {
+						int y= (int) e.getItem();
+						startDate=startDate.withYear(y);
+						if(radio[0].isSelected()){
+			               
+			                get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+			                get.setVisible(true);
+			            }
+						else if(radio[1].isSelected()) {
+							
+			                get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+			                get.setVisible(true);
+						}
+					}
+				});
+				
+				monthCombo2.addItemListener(new ItemListener() {
+					public void itemStateChanged(ItemEvent e) {
+						int m= (int) e.getItem();
+						startDate=startDate.withMonth(m);
+						if(radio[0].isSelected()){
+			             
+			                get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+			                get.setVisible(true);
+			            }
+						else if(radio[1].isSelected()) {
+							
+			                get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+			                get.setVisible(true);
+						}
+					}
+				});
+
+				
+				dayCombo2.addItemListener(new ItemListener() {
+					public void itemStateChanged(ItemEvent e) {
+						int d= (int) e.getItem();
+						startDate=startDate.withDayOfMonth(d);
+						if(radio[0].isSelected()){
+			             
+			                get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+			                get.setVisible(true);
+			            }
+						else if(radio[1].isSelected()) {
+							
+			                get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+			                get.setVisible(true);
+						}
+					}
+				});
+				
+		
+		
+		//더하거나 뺄 꺼 선택할 때 이벤트
+		yearCombo3.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				ay= (int) e.getItem();
+				if(radio[0].isSelected()){
+	                
+	                get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+	                get.setVisible(true);
+	            }
+				else if(radio[1].isSelected()) {
+				 
+	                get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+	                get.setVisible(true);
+				}
+				
+			}
+		});
+		
+		monthCombo3.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				am= (int) e.getItem();
+				if(radio[0].isSelected()){
+	            
+	                get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+	                get.setVisible(true);
+	            }
+				else {
+					
+					get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+	                get.setVisible(true);
+				}
+			}
+		});
+
+		
+		dayCombo3.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				ad= (int) e.getItem();
+				if(radio[0].isSelected()){
+	              
+	                get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+	                get.setVisible(true);
+	            }
+				else {
+			
+					get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+	                get.setVisible(true);
+				}
+			}
+		});
+		
+		 
+		
+		//라디오 버튼 이벤트 - 빼기에서 추가로 바뀔 때 바로 빼져야 함
+		radio[0].addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				radio[1].setSelected(false);
+				  get.setText((AddDate(startDate,ay,am,ad)).format(formatter)+getd);
+	              get.setVisible(true);
+			}
+		});
+		radio[1].addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				radio[0].setSelected(false);
+				 get.setText(MinusDate(startDate,ay,am,ad).format(formatter)+getd);
+	                get.setVisible(true);
+			}
+		});
+		
 		
 		JLabel yLbl3 = new JLabel("년 "); 
 		p2.add(yLbl3).setBounds(20,162,20,20);
@@ -313,15 +732,6 @@ public class GUI_date extends DateCalc {
 		JLabel nalza = new JLabel("날짜 "); 
 		p2.add(nalza).setBounds(20,222,40,20);
 		
-		
-		//나중에 다시 - 이벤트 처리 - 걍 위치만 
-		
-		LocalDate dd=LocalDate.of( (int) yearCombo2.getSelectedItem(),(int) monthCombo2.getSelectedItem(),(int)dayCombo2.getSelectedItem());
-		DateCalc.AddDate(dd,ay,am,ad);
-		
-		JLabel get=new JLabel(dd.format(DateTimeFormatter.ofPattern(" yyyy년 MM월 dd일 "))+getd+"요일");
-		p2.add(get).setBounds(20,235,160,30);
-		//
 	
 		
 		f.add(p2);

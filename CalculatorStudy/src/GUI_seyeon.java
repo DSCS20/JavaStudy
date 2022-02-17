eb984b1ad799ba0f768313400b272ecd8af5b3b9
LAPTOP-RJHC46RB\seyeo <seyeon3178@duksung.ac.kr>
2022-02-17 13:49:42 +0900

    계산기

--- CalculatorStudy/src/GUI_seyeon.java
+++ CalculatorStudy/src/GUI_seyeon.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,6 +8,8 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class GUI_seyeon extends JFrame implements ActionListener{
 	
@@ -20,6 +23,13 @@ public class GUI_seyeon extends JFrame implements ActionListener{
 	private JLabel str; //숫자위에
 	private JButton btn[];
 	private JPanel mainPanel,bitPanel,buttonPanel,calcPanel;
+
+	String result="";  //null로 넣으면 오류
+	String result1=""; //중간
+	String result2=""; //현재화면
+	String bun[]= {"","","",""};//0은 결과, 1은 중간, 2는 현재, 3은 진수변환값
+	String tr="";//진수변환용
+	int cnt=0;
 	
 	public GUI_seyeon() {
 		setBackground(Color.gray);
@@ -37,7 +47,7 @@ public class GUI_seyeon extends JFrame implements ActionListener{
 		
 		mainPanel.setLayout(new GridLayout(2,1));
 		
-		str=new JLabel("0+0",JLabel.RIGHT);
+		str=new JLabel("",JLabel.RIGHT);
 		output=new JLabel("0",JLabel.RIGHT);
 		str.setBackground(Color.WHITE);
 		output.setBackground(Color.WHITE);
@@ -96,10 +106,11 @@ public class GUI_seyeon extends JFrame implements ActionListener{
 		
 		bitPanel.setLayout(new GridLayout(2,4,2,2));
 		
-		for(int i=34;i<=37;i++) {
+		for(int i=34;i<=37;i++) { //진수
 			bitPanel.add(btn[i]);
 		}
-		for(int i=25;i<=28;i++) {
+		
+		for(int i=25;i<=28;i++) { //논리
 			bitPanel.add(btn[i]);
 		}
 		
@@ -149,14 +160,16 @@ public class GUI_seyeon extends JFrame implements ActionListener{
 			}
 			btn[i].setBackground(Color.DARK_GRAY);
 			btn[i].setForeground(Color.WHITE);
+			
+			btn[i].addActionListener(this);
 		}
 
 		calcPanel.setLayout(new BorderLayout());
 		calcPanel.add(bitPanel,BorderLayout.NORTH);
-		calcPanel.add(buttonPanel,BorderLayout.SOUTH);
+		calcPanel.add(buttonPanel,BorderLayout.CENTER);
 		
 		getContentPane().add(mainPanel,BorderLayout.NORTH);
-		getContentPane().add(calcPanel,BorderLayout.SOUTH);
+		getContentPane().add(calcPanel,BorderLayout.CENTER);
 		
 	}
 
@@ -169,8 +182,306 @@ public class GUI_seyeon extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		String data=e.getActionCommand();
 		
+		if(data.equals("1")||data.equals("2")||data.equals("3")||data.equals("4")||
+				data.equals("5")||data.equals("6")||data.equals("7")||data.equals("8")||
+				data.equals("9")) {
+			result2+=data;
+			output.setText(result2);
+		}
+		else if(data.equals("←")) {
+			int len=output.getText().length();
+			if(len==1)
+			{
+				result2="";
+				output.setText("0");
+			}
+			else
+			{
+				if(!result2.equals(""))
+				{
+					result2=result2.substring(0,len-1);
+					output.setText(result2);
+				}
+				
+			}
+		}
+		else if(data.equals("CE"))
+		{
+			result="";
+			result1="";
+			result2="";
+			str.setText("");
+			output.setText("0");
+			bun[0]="";
+			bun[1]="";
+			bun[2]="";
+		}
+		else if(data.equals("."))
+		{
+			if(result2.equals(""))
+			{
+				if(result2.indexOf(".")==-1)
+				{
+					result="0"+data;
+				}
+			}
+			else 
+			{
+				if(result2.indexOf(".")==-1)
+				{
+					result+=data;
+				}
+			}
+			output.setText(result2);
+		}
+		else if(data.equals("0"))
+		{
+			if(result2.equals("0"))
+			{
+				result="0";
+			}
+			else
+			{
+				result2+=data;
+			}
+			output.setText(result2);
+		}
+		else if(data.equals("*")||data.equals("/")||data.equals("+")||data.equals("-")||
+				data.equals("AND")||data.equals("OR")||data.equals("NOT")||data.equals("XOR")||
+				data.equals("%"))
+		{
+			if(result1.equals(""))
+			{
+				if(result2.equals(""))
+				{
+					if(result.equals(""))
+					{
+						result1="0"+data;
+						bun[0]="0";
+					}
+					else
+					{
+						result1=result+data;
+					}
+				}
+				else
+				{
+					if(!result.equals(""))
+					{
+						result1=result+data;
+					}
+					result1=result2+data;
+					bun[0]=result2;
+				}
+				bun[1]=data;
+			}
+			else
+			{
+				if(result2.equals(""))
+				{
+					result1=result1.substring(0,result1.length()-1)+data;
+					bun[1]=data;
+				}
+				else
+				{
+					if(cnt>=1)
+					{
+						if(data.equals("*")||data.equals("/")||data.equals("%"))
+						{
+							result2="1";
+						}
+						else
+							result2="0";
+					}
+					result1+=result2+data;
+					bun[2]=result2;
+					result=Calc();
+					bun[1]=data;
+					output.setText(result);
+					bun[0]=result;
+				}
+				cnt=0;
+			}
+			if(result.equals("0으로 나눌 수 없다"))
+			{
+				result1 = "";
+				result2 = "";
+				result = "";
+				bun[0] = "";
+				bun[1] = "";
+				bun[2] = "";
+			}
+			result2="";
+			str.setText(result1);
+		}
+
+		else if(data.equals("="))
+		{
+			cnt+=1;
+			if(!result2.equals(""))
+			{
+				bun[2]=result2;
+				str.setText("");
+			}
+			bun[0]=Calc();
+			output.setText(bun[0]);
+			if(bun[1].equals("/") && result2.equals("0"))
+			{
+				result1 = "";
+				result2 = "";
+				result = "";
+				bun[0] = "";
+				bun[1] = "";
+				bun[2] = "";
+				output.setText("0으로 나눌 수 없다");
+			}
+		}
+		//else if
+		
+	}
+	
+	public String Calc() {
+		String sum ="";
+		BigDecimal big1 = new BigDecimal(bun[0]);
+		BigDecimal big2 = new BigDecimal(bun[2]);
+		Double dou=new Double(0);
+		if(bun[1].equals("*"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				dou = big1.multiply(big2,MathContext.DECIMAL32).doubleValue();//바로 toString에 넣으면 뒤에 .0000이 잘리지 않는다.
+				sum += dou.toString();
+			}
+			else
+				sum += Integer.parseInt(bun[0]) * Integer.parseInt(bun[2]);
+		}
+		if(bun[1].equals("-"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				dou = big1.subtract(big2,MathContext.DECIMAL32).doubleValue();
+				sum += dou.toString();
+			}
+			else
+				sum += Integer.parseInt(bun[0]) - Integer.parseInt(bun[2]);
+		}
+		if(bun[1].equals("+"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				dou = big1.add(big2,MathContext.DECIMAL32).doubleValue();
+				sum += dou.toString();
+			}
+			else
+				sum += Integer.parseInt(bun[0]) + Integer.parseInt(bun[2]);
+		}
+		if(bun[1].equals("/"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				try
+				{
+					dou = big1.divide(big2,MathContext.DECIMAL32).doubleValue();
+					sum += dou.toString();
+				}
+				catch (ArithmeticException e)
+				{
+					sum += "0으로 나눌 수 없다";
+				}
+				
+			}
+			else
+			{
+				try
+				{
+					sum += Integer.parseInt(bun[0]) / Integer.parseInt(bun[2]);
+				}
+				catch (ArithmeticException e)
+				{
+					sum += "0으로 나눌 수 없다";
+				}
+				
+			}
+		}
+		if(bun[1].equals("%"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				try
+				{
+					dou = big1.divide(big2,MathContext.DECIMAL32).doubleValue();
+					sum += dou.toString();
+				}
+				catch (ArithmeticException e)
+				{
+					sum += "0으로 나눌 수 없다";
+				}
+				
+			}
+			else
+			{
+				try
+				{
+					sum += Integer.parseInt(bun[0]) % Integer.parseInt(bun[2]);
+				}
+				catch (ArithmeticException e)
+				{
+					sum += "0으로 나눌 수 없다";
+				}
+				
+			}
+			
+		}
+		if(bun[1].equals("AND"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				dou = big1.add(big2,MathContext.DECIMAL32).doubleValue();
+				sum += dou.toString();
+			}
+			else
+				sum += Integer.parseInt(bun[0]) & Integer.parseInt(bun[2]);
+		}
+		if(bun[1].equals("OR"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				dou = big1.add(big2,MathContext.DECIMAL32).doubleValue();
+				sum += dou.toString();
+			}
+			else
+				sum += Integer.parseInt(bun[0]) | Integer.parseInt(bun[2]);
+		}
+		if(bun[1].equals("NOT"))//NOT은 다시
+		{
+			if(bun[0].indexOf(".") != -1)
+			{
+				dou = big1.add(big2,MathContext.DECIMAL32).doubleValue();
+				sum += dou.toString();
+			}
+			else
+				sum += ~Integer.parseInt(bun[0]);
+		}
+		if(bun[1].equals("AND"))
+		{
+			if(bun[0].indexOf(".") != -1 || bun[2].indexOf(".") != -1)
+			{
+				dou = big1.add(big2,MathContext.DECIMAL32).doubleValue();
+				sum += dou.toString();
+			}
+			else
+				sum += Integer.parseInt(bun[0]) ^ Integer.parseInt(bun[2]);
+		}
+		return sum;
+
+	}
+	
+	public String Trans()
+	{
+		String result = "";
+		return result;
 	}
 	
 }	
